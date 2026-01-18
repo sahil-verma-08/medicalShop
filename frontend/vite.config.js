c055986201532ab650e957fba31359804b1351ae
sahil-verma-08 <sahilverma2431@gmail.com>
2026-01-18 15:58:27 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: '/',
   server: mode === 'development'
     ? {
         port: 3000,
